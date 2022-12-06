e7ff568502101644b3d9684ee706180d49a781ea
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-06 15:59:44 +0530

    Inserting element inside linkedlist in a particular position

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -10,7 +10,7 @@ namespace LinkedList
     {
         public Node head;
 
-        public void Append(int data)
+        public void Add(int data)
         {
             Node node = new Node(data);
             if (this.head == null)
@@ -26,7 +26,61 @@ namespace LinkedList
                 }
                 temp.next = node;
             }
-            Console.WriteLine($"{node.data} Append in to LinkedList");
+            Console.WriteLine($"{node.data} Add in to LinkedList");
         }
+        public Node InserttoLnkLst(int position, int data)
+        {
+            if (position < 0)
+            {
+                Console.WriteLine("Invalid position : Write a valid position");
+            }
+            if (position == 1)
+            {
+                Node newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+                
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("position Out Of Range");
+                }
+            }
+            return head;
+        }
+        public void DiaplayLnkLst()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("The LinkedList is Emptay");
+                return;
+            }
+            else
+            {
+                
+                while (temp != null)
+                {
+                    Console.Write(temp.data + "  ");
+                    temp = temp.next;
+                }
+                
+            }
+        }
+
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,9 +7,14 @@
             Console.WriteLine("Creating Project For LinkedList Program");
 
             LinkedList myLinkedList = new LinkedList();
-            myLinkedList.Append(30);
-            myLinkedList.Append(56);
-            myLinkedList.Append(70);
+            myLinkedList.Add(30);
+            myLinkedList.Add(56);
+            myLinkedList.Add(70);
+            myLinkedList.Add(10);
+            myLinkedList.Add(20);
+            myLinkedList.Add(40);
+            myLinkedList.InserttoLnkLst(5, 69);
+            myLinkedList.DiaplayLnkLst();
         }
     }
 }
