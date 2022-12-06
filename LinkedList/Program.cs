2709480faa23cb11a6e244b11808da867a3cb3cc
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-06 13:40:09 +0530

    Creating a simple Linked List of 56, 30 and 70

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Creating Project For LinkedList Program");
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+
+            foreach (int item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
